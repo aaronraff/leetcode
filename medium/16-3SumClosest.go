174a764f30c54b7668a9f576d8df198e50bbbb1f
Aaron Raff <aaronraffdev@gmail.com>
2020-01-01 23:03:50 -0500

    add solution to 3Sum Closest

---
+++ medium/16-3SumClosest.go
@@ -0,0 +1,29 @@
+func threeSumClosest(nums []int, target int) int {
+    var left, right, sum int
+    closestDist := math.Inf(1)
+    closestVal := 0
+    sort.Ints(nums)
+    
+    for index := 0; index < len(nums)-2; index++ {
+        left = index+1
+        right = len(nums)-1
+        
+        for left < right {
+            sum = nums[index] + nums[left] + nums[right]
+            dist := math.Abs(float64(target - sum))
+        
+            if dist < closestDist {
+                closestDist = dist
+                closestVal = sum
+            }
+
+            if sum > target {
+                right--
+            } else {
+                left++
+            }
+        }
+    }
+    
+    return closestVal
+}
