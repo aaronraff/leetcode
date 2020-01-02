7e5e775b6887b51635fd860b387d3dbad094742c
Aaron Raff <aaronraffdev@gmail.com>
2020-01-01 23:10:18 -0500

    remove unnecessary code

--- medium/16-3SumClosest.go
+++ medium/16-3SumClosest.go
@@ -1,15 +1,14 @@
 func threeSumClosest(nums []int, target int) int {
-    var left, right, sum int
     closestDist := math.Inf(1)
     closestVal := 0
     sort.Ints(nums)
     
     for index := 0; index < len(nums)-2; index++ {
-        left = index+1
-        right = len(nums)-1
+        left := index+1
+        right := len(nums)-1
         
         for left < right {
-            sum = nums[index] + nums[left] + nums[right]
+            sum := nums[index] + nums[left] + nums[right]
             dist := math.Abs(float64(target - sum))
         
             if dist < closestDist {
