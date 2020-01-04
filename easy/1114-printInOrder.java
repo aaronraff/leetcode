e9b84fcf7f334184e187c11bef53faa9e584db0f
Aaron Raff <aaronraffdev@gmail.com>
2020-01-04 11:37:32 -0500

    add "this." for better consistency

--- easy/1114-printInOrder.java
+++ easy/1114-printInOrder.java
@@ -10,19 +10,19 @@ class Foo {
     public void first(Runnable printFirst) throws InterruptedException {
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        s2.release();
+        this.s2.release();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
         // printSecond.run() outputs "second". Do not change or remove this line.
-        s2.acquire();
+        this.s2.acquire();
         printSecond.run();
-        s3.release();
+        this.s3.release();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
         // printThird.run() outputs "third". Do not change or remove this line.
-        s3.acquire();
+        this.s3.acquire();
         printThird.run();
     }
 }
